f2daebfb78ca436e5f7a0faf1dc3fe89fa4c2f19
ConditionalStatementLifestyle <github email address>
2019-04-18 15:49:58 -0700

    more nav bar stuff and routing

--- hike-candy/app/controllers/feed_controller.rb
+++ hike-candy/app/controllers/feed_controller.rb
@@ -3,8 +3,6 @@ class FeedController < ApplicationController
   def feed
     @posts = Post.last(5)
     @trips = Trip.last(5)
-
-    
   end
 
 end
--- hike-candy/app/controllers/welcome_controller.rb
+++ hike-candy/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
 
+  def welcome
+    if session[:username] != nil
+      redirect_to "/feed"
+    end
+  end
 
 end
