8f9e0de1b06959db3a1386ffd328eda36fa8c037
quinncidences <qrc.inpa@gmail.com>
2019-04-18 09:14:10 -0700

    fixed follower counts

--- hike-candy/app/controllers/users_controller.rb
+++ hike-candy/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
 
-    current_user = User.find(session[:user_id])
+    current_user = User.find(params[:id])
     @following_relationships = current_user.active_relationships
     @follower_relationships = current_user.passive_relationships
     @followingcount = @following_relationships.count
